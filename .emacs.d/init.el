5e94a3f65f1649ef993d35ca440158df754eb263
Zhijia on x61 <z.jia.hu@gmail.com>
2015-09-02 19:56:03 +0800

    Update init.el
    Install packages defined in init.el if not yet installed
    Added js2-mod

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,34 @@
    t)
   (package-initialize))
 
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+(ensure-package-installed 'monokai-theme
+                          'magit
+                          'helm-projectile
+                          'js2-mode,
+                          'web-beautify)
+
+;; activate installed packages
+(package-initialize)
+
 ;; Load theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'monokai t)
@@ -136,3 +164,6 @@
 
 ;; Toogle header/implementation file
 (global-set-key (kbd "C-c o") 'ff-find-other-file)
+
+;; js2 settings
+(js2-imenu-extras-mode)
