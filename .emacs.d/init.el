8e6b2c8477c17b288a8d17a9819010d8cb51b73d
Lee Ming Lim <leeminglim@Lees-MacBook-Pro.local>
2015-09-22 22:13:30 +0800

    removed erc tracking on by default

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -217,7 +217,6 @@
 (setq erc-autojoin-channels-alist
       '((".*\\.freenode.net" "#emacs" "#python")))
 (setq erc-nick "aijihz") 
-(erc-track-mode t)
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
                                  "324" "329" "332" "333" "353" "477"))
 (setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
