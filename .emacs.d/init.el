caee9c352596b26432b95aae60ebe4ef243a947e
Zhijia <zhijia.hu@ubisoft.com>
2015-10-23 17:03:34 +0800

    Added html settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -237,3 +237,10 @@
 
 (setq org-plantuml-jar-path
       (expand-file-name "~/.emacs.d/plantuml.jar"))
+
+;; Html settings
+(add-hook 'html-mode-hook
+          (lambda ()
+            ;; Default indentation is usually 2 spaces, changing to 4.
+            (set (make-local-variable 'sgml-basic-offset) 4)))
+
