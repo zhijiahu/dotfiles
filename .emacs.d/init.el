41fbf34a93ade758c9eff02ff2acdb04c4eac26b
Zhijia Hu <Zhijia Hu>
2016-10-15 08:31:56 +0800

    always install package, don't ask

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,9 +18,7 @@
      ;; (package-installed-p 'evil)
      (if (package-installed-p package)
          nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
+       (package-install package)))
    packages))
 
 ;; make sure to have downloaded archive description.
